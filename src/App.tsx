e9e682cd8eccf723db6de53b19ad8aa5280885b8
Mourad Elfeleh <felehdev@gmail.com>
2025-02-03 13:53:41 +0100

    Feature/5 refactor resume section (#16)
    * docs: update README with convention commit message type
    
    * feat: add home component
    
    * build: install bootstrap
    
    * buid: install sass
    
    * feat: create main.scss and style Home component
    
    * buid: install typed.js
    
    * feat: add useInitApp hook to initialize Typed.js on page load
    
    * feat: add Contact component
    
    * feat: style Contact component
    
    * feat: add Quotes component
    
    * feat: style Quotes component
    
    * feat: add Skills component
    
    * feat: style Skills component
    
    * feat: add Resume component
    
    * feat: style Resume component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import "./assets/styles/main.scss";
 import Home from "./components/Home";
+import Skills from "./components/Skills";
+import Resume from "./components/Resume";
 import Quotes from "./components/Quotes";
 import Contact from "./components/Contact";
-import Skills from "./components/Skills";
 
 function App() {
   return (
     <>
       <Home />
       <Skills />
+      <Resume />
       <Quotes />
       <Contact />
     </>
