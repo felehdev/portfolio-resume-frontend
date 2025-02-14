eae2b7e3f5afa794331f8f969e33a790c97eeb16
felehdev <felehdev@gmail.com>
2025-02-15 00:32:06 +0100

    feat: integrate perfect-scrollbar for smooth scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import PerfectScrollbar from "perfect-scrollbar";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Home from "./components/Home";
@@ -8,6 +9,8 @@ import Skills from "./components/Skills";
 import "./assets/styles/main.scss";
 
 function App() {
+  new PerfectScrollbar("#root", {});
+
   return (
     <>
       <Home />
